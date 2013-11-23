d383ba48ed03507cc7f2fe5df366ae3241c4cf81
Bonnie Barrilleaux <bbarrill@ucdavis.edu>
2013-11-23 10:14:17 -0800

    trying to fix file path for saving temp files

--- web.rb
+++ web.rb
@@ -5,6 +5,7 @@ require 'pry'
 require 'base64'
 
 set :slim, :pretty => true
+set :root, File.dirname(__FILE__)
 
 get '/' do
   slim :index
@@ -17,11 +18,11 @@ post '/' do
   puts @start_column
   R.eval <<EOF
    data <- rnorm(100, #{chromosome_column})
-   png("stuff.png")
+   png("./stuff.png")
     hist(data, main="Here's a demo graph.", xlab="Centered on the chromosome column you entered.")
    dev.off()
 EOF
-  @data_uri = Base64.strict_encode64(File.open("stuff.png", "rb").read)
+  @data_uri = Base64.strict_encode64(File.open("./stuff.png", "rb").read)
   imagetag = '<img alt="graph" src="data:image/png;base64,%s">' % @data_uri
 
   html = "<html>"
