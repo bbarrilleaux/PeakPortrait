3cf93085f57e460711a6a5af75542f0d6765b6e4
Bonnie Barrilleaux <bbarrill@ucdavis.edu>
2013-11-23 10:42:49 -0800

    added temp path

--- web.rb
+++ web.rb
@@ -12,17 +12,20 @@ get '/' do
 end
 
 post '/' do
+  path="./tmp"
+  FileUtils.mkdir(path) unless File.exists?(path)
+
   @start_column = params[:start_column]
   chromosome_column = params[:chromosome_column]
   puts @chromosome_column
   puts @start_column
   R.eval <<EOF
    data <- rnorm(100, #{chromosome_column})
-   png("./stuff.png")
+   png("./tmp/stuff.png")
     hist(data, main="Here's a demo graph.", xlab="Centered on the chromosome column you entered.")
    dev.off()
 EOF
-  @data_uri = Base64.strict_encode64(File.open("./stuff.png", "rb").read)
+  @data_uri = Base64.strict_encode64(File.open("./tmp/stuff.png", "rb").read)
   imagetag = '<img alt="graph" src="data:image/png;base64,%s">' % @data_uri
 
   html = "<html>"
