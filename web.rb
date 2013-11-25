3246a80bf383eab7788f2ee7f7f69f6224f590f9
biobonnie <bbarrill@ucdavis.edu>
2013-11-25 14:24:18 -0800

    use graph.slim template to display graph

--- web.rb
+++ web.rb
@@ -16,22 +16,17 @@ post '/' do
   FileUtils.mkdir(path) unless File.exists?(path)
 
   @start_column = params[:start_column]
-  chromosome_column = params[:chromosome_column]
+  @chromosome_column = params[:chromosome_column]
   puts @chromosome_column
   puts @start_column
   R.eval <<EOF
-   data <- rnorm(100, #{chromosome_column})
-   png("./tmp/stuff.png", type="cairo")
+   data <- rnorm(100, #{@chromosome_column})
+   png("./tmp/graph.png", type="cairo-png")
     hist(data, main="Here's a demo graph.", xlab="Centered on the chromosome column you entered.")
    dev.off()
 EOF
-  @data_uri = Base64.strict_encode64(File.open("./tmp/stuff.png", "rb").read)
-  imagetag = '<img alt="graph" src="data:image/png;base64,%s">' % @data_uri
+  @data_uri = Base64.strict_encode64(File.open("./tmp/graph.png", "rb").read)
 
-  html = "<html>"
-  html += "<head><title>Your Chrainbow is ready!</title></head>"
-  html += "<body>"
-  html += imagetag
-
-#  slim :task
+ File.delete("./tmp/graph.png")
+  slim :graph
 end
