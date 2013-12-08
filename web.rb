04307420992c4413b1dc6147b59a2b2f96166bc9
Bonnie Barrilleaux <bbarrill@ucdavis.edu>
2013-12-07 18:17:18 -0800

    extract user submission to model

--- web.rb
+++ web.rb
@@ -9,6 +9,54 @@ set :slim, :pretty => true
 set :root, File.dirname(__FILE__)
 
 
+class ChromosomeDataFile
+  def initialize(params)
+    @params = params
+    raise "You didn't select a file?" unless @params[:file]
+  end
+
+  def gff?
+    @params[:file_type] == "GFF"
+  end
+
+  def start_column
+    gff? ? 4 : 2
+  end
+
+  def end_column
+    return 0 if @params[:use_peak_widths] == "no"
+    gff? ? 5 : 3
+  end
+
+  def score_column
+    if @params[:use_score] == "no"
+      return 0
+    elsif @params[:use_score] == "5 (BED/broadPeak)"
+      return 5
+    elsif @params[:use_score] == "6 (GFF)"
+      return 6
+    else
+      return @params[:use_score].to_i
+    end
+  end
+
+  def write_temp_file(file_path)
+    temp_file = @params[:file][:tempfile]
+    FileUtils.mkdir_p(File.dirname(file_path))
+    dest_file = File.open(file_path, "w")
+    file_contents = temp_file.read
+    file_contents.gsub!(/\r\n?/, "\n") # make sure line endings are consistent
+    file_contents.each_line do |line|
+        # only keep lines that start with "chr". other lines are probably headers/comments/junk.
+        dest_file.write(line) if /^chr/.match(line) 
+    end
+  end
+
+  def delete_temp_file(file_path)
+    File.delete(file_path)
+  end
+end
+
 get '/styles.css' do
   scss :styles
 end
@@ -18,98 +66,71 @@ get '/' do
 end
 
 post '/' do
-  path="./tmp"
-  FileUtils.mkdir(path) unless File.exists?(path)
+  data_file = ChromosomeDataFile.new params
 
-  if params[:file_type] == "GFF"
-    R.start_column = 4
-    R.end_column = 5
-  else
-    R.start_column = 2
-    R.end_column = 3
-  end
+  R.start_column = data_file.start_column
+  R.end_column = data_file.end_column
+  R.score_column = data_file.score_column
 
-  if params[:use_peak_widths] == "no"
-    R.end_column = 0
-  end
-
-  if params[:use_score] == "no"
-    R.score_column = 0
-  elsif params[:use_score] == "5 (BED/broadPeak)"
-    R.score_column = 5
-  elsif params[:use_score] == "6 (GFF)"
-    R.score_column = 6
-  else
-    R.score_column = params[:use_score].to_i
-  end
-
-  if params[:file]
-    puts params[:file]
-    tempfile = params[:file][:tempfile]
-    filename = params[:file][:filename]
-
-    inputfile = File.open("./tmp/input.txt", "w")
-    text = tempfile.read
-    text.gsub!(/\r\n?/, "\n") #make sure line endings are consistent
-    text.each_line do |line|
-            #only keep lines that start with "chr". other lines are probably headers/comments/junk.
-            inputfile.write(line) if /^chr/.match(line) 
-    end
-  else
-    raise "You didn't select a file?"
-  end
+  temp_file_path = "./tmp/input.txt"
+  R.file = temp_file_path
+  data_file.write_temp_file(temp_file_path)
  
   R.eval <<EOF
     library(ggplot2)
-    library(gtools) #for reordering chromosome names
+    library(gtools) # for reordering chromosome names
     
-    peakfile <- read.table("./tmp/input.txt",sep="\t",header=FALSE)
-    if(is.factor(peakfile[,start_column])) { #detect header line if it exists
-      peakfile <- read.table("./tmp/input.txt",sep="\t",header=TRUE)
+    peaks <- read.table(file, sep = "\t", header = FALSE)
+    # detect header line if it exists
+    if (is.factor(peaks[, start_column])) { 
+      peaks <- read.table(file, sep = "\t", header = TRUE)
     }
 
-    data <- data.frame(as.factor(peakfile[,1]), peakfile[,start_column])
+    data <- data.frame(as.factor(peaks[, 1]), peaks[, start_column])
 
-    if(end_column && !score_column) { 
-      data <- cbind(data, peakfile[,end_column] - peakfile[,start_column]) 
+    if (end_column && !score_column) { 
+      data <- cbind(data, peaks[, end_column] - peaks[, start_column]) 
     } else if (!end_column && score_column) {
-      data <- cbind(data, peakfile[,score_column])
+      data <- cbind(data, peaks[, score_column])
     } else if (end_column && score_column) {
-      data <- cbind(data, (peakfile[,end_column] - peakfile[,start_column]) * peakfile[,score_column] )
+      data <- cbind(data, 
+                    (peaks[, end_column] - peaks[, start_column]) * peaks[, score_column])
     } else {
-      data <- cbind(data, rep(1,nrow(data)))
+      data <- cbind(data, rep(1, nrow(data)))
     }
 
     names(data) <- c("Chromosome", "loc", "size")
 
-    if("chr20" %in% data$Chromosome | "chr21" %in% data$Chromosome | "chr22" %in% data$Chromosome) {
+    if ("chr20" %in% data$Chromosome | 
+        "chr21" %in% data$Chromosome | 
+        "chr22" %in% data$Chromosome) {
       species = "human"
-      data <- rbind(data, read.table("./HumanChromosomeLengths.txt",sep="\t",header=TRUE))
-    } else if ("chr19" %in% data$Chromosome | "chr18" %in% data$Chromosome | "chr17" %in% data$Chromosome) {
+      data <- rbind(data, read.table("./HumanChromosomeLengths.txt", sep = "\t", header = TRUE))
+    } else if ("chr19" %in% data$Chromosome | 
+               "chr18" %in% data$Chromosome | 
+               "chr17" %in% data$Chromosome) {
       species = "mouse"
-      data <- rbind(data, read.table("./MouseChromosomeLengths.txt",sep="\t",header=TRUE))
+      data <- rbind(data, read.table("./MouseChromosomeLengths.txt", sep = "\t", header = TRUE))
     } else {
       species = "unknown"
     }
 
     data$Chromosome <- factor(data$Chromosome, mixedsort(levels(data$Chromosome)))
-    theme_set(theme_gray(base_size = 18)) #make fonts bigger
+    theme_set(theme_gray(base_size = 18)) # make fonts bigger
 
     png("./tmp/graph.png", type="cairo-png", width = 900, height=600)
 
-      hist_results <- ggplot(data, aes(x=loc/1000000, colour=Chromosome, weight=size))
+      hist_results <- ggplot(data, aes(x = loc/1000000, colour = Chromosome, weight = size))
       hist_results + geom_freqpoly() + 
-        xlab("Distance along chromosome (Mbp)") + ylab("Density") +
-        facet_wrap("Chromosome", drop=FALSE, scales="free_x")
+        xlab("Position along chromosome (Mbp)") + ylab("Density") +
+        facet_wrap("Chromosome", drop = FALSE, scales = "free_x")
    dev.off()
 
 EOF
 
   @species = R.species
 
-  if File.exists?("./tmp/input.txt")
-    File.delete("./tmp/input.txt")
-  end
+  data_file.delete_temp_file(temp_file_path)
 
   if File.exists?("./tmp/graph.png")
     @data_uri = Base64.strict_encode64(File.open("./tmp/graph.png", "rb").read)
